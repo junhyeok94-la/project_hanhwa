7f452dd432b7d2f00be7a9a11c85904d1148c2d1
junhyeok-InnerPiece <najunhyeok94@gmail.com>
2025-08-24 16:09:05 +0900

    Create main.py (모니터링 웹서버의 main 용도)

---
+++ airflow/main.py
@@ -0,0 +1,10 @@
+# home.py 파일 내용
+import streamlit as st
+
+st.set_page_config(
+    page_title="통합 모니터링",
+    layout="wide"
+)
+
+st.title("통합 모니터링 대시보드")
+st.markdown("왼쪽 사이드바에서 원하는 모니터링 페이지를 선택해 주세요.")
